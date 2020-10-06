9039eeeabf8c415293d2958b832d8475772d9eb1
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-06 13:14:30 +0000

    made the code print the numbers a user entered in a sequence on a single line

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program thats counts from 1 to a user entered number in a single line
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +10,22 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for a positive integer
+    System.out.println("Please enter a positive integer");
+    int n = input.nextInt();
+
+    // create a for loop
+    for(int count = 1; count <= n; count++){
+      // make it so the last number doesnt have a comma
+      if(count < n){
+        System.out.print(count + ",");
+      } else {
+        System.out.print(count);
+      }
+    }
     
   }
 }
